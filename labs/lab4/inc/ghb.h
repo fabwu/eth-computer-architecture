7e638827212e58df0a2c82244c4f19f708610e77
Fabian Wüthrich <fabianwu@student.ethz.ch>
2020-12-20 17:30:31 +0100

    Add feedback

--- labs/lab4/inc/ghb.h
+++ labs/lab4/inc/ghb.h
@@ -22,16 +22,42 @@ struct GHB_Entry {
 
 class GHB {
   public:
+    enum aggressivness_t {
+        VERY_CONSERVATIVE,
+        CONSERVATIVE,
+        MIDDLE_OF_THE_ROAD,
+        AGGRESSIVE,
+        VERY_AGGRESSIVE,
+
+        NUM_AGGRESSIVNESS
+    };
+
     void operate(uint64_t addr, uint64_t ip, std::vector<uint64_t> *pf_addr_list);
+    void set_aggressiveness(aggressivness_t aggressivness);
 
     GHB() {
         it_head_idx = 0;
         ghb_head_idx = 0;
+        // default is 4 so Task 1 still works
         prefetch_distance = 4;
         prefetch_degree = 4;
+        aggressivness = MIDDLE_OF_THE_ROAD;
+
+        pf_distance_table[VERY_CONSERVATIVE] = 4;
+        pf_distance_table[CONSERVATIVE] = 8;
+        pf_distance_table[MIDDLE_OF_THE_ROAD] = 16;
+        pf_distance_table[AGGRESSIVE] = 32;
+        pf_distance_table[VERY_AGGRESSIVE] = 48;
+
+        pf_degree_table[VERY_CONSERVATIVE] = 1;
+        pf_degree_table[CONSERVATIVE] = 1;
+        pf_degree_table[MIDDLE_OF_THE_ROAD] = 2;
+        pf_degree_table[AGGRESSIVE] = 4;
+        pf_degree_table[VERY_AGGRESSIVE] = 4;
     }
 
   private:
+    aggressivness_t aggressivness;
     uint64_t prefetch_distance;
     uint64_t prefetch_degree;
 
@@ -40,6 +66,9 @@ class GHB {
 
     GHB_Entry ghb[GHB_SIZE];
     int ghb_head_idx;
+
+    int8_t pf_distance_table[NUM_AGGRESSIVNESS];
+    int8_t pf_degree_table[NUM_AGGRESSIVNESS];
 };
 
 void GHB::operate(uint64_t addr, uint64_t ip, std::vector<uint64_t> *pf_addr_list) {
@@ -140,6 +169,15 @@ void GHB::operate(uint64_t addr, uint64_t ip, std::vector<uint64_t> *pf_addr_lis
             }
         }
     }
+}
+
+void GHB::set_aggressiveness(aggressivness_t a) {
+    if (aggressivness != a) {
+        cout << "aggressivness changed from " << aggressivness << " to " << a << endl;
+    }
+    aggressivness = a;
+    prefetch_distance = pf_distance_table[aggressivness];
+    prefetch_degree = pf_degree_table[aggressivness];
 }
 
 #endif
