10a316ed76d0f58c89f8d9e3b0bd77bfa594d3d5
Fabian Wüthrich <fabianwu@student.ethz.ch>
2020-12-21 18:07:32 +0100

    Add feedback euler

--- labs/lab4/run_euler.sh
+++ labs/lab4/run_euler.sh
@@ -4,3 +4,4 @@ bsub -J "no[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_
 bsub -J "next[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_next | bash"
 bsub -J "stride[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_stride | bash"
 bsub -J "ghb[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_ghb | bash"
+bsub -J "fbp[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_fbp | bash"
