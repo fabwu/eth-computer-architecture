2fb686fd8e5589120a641795f4503535281c7a96
Fabian Wüthrich <fabianwu@student.ethz.ch>
2021-01-06 16:27:32 +0100

    Add euler direct index results

--- labs/lab4/run_euler.sh
+++ labs/lab4/run_euler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-bsub -J "no[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_no | bash"
-bsub -J "next[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_next | bash"
-bsub -J "stride[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_stride | bash"
+#bsub -J "no[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_no | bash"
+#bsub -J "next[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_next | bash"
+#bsub -J "stride[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_stride | bash"
 bsub -J "ghb[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_ghb | bash"
-bsub -J "fbp[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_fbp | bash"
+bsub -J "fbp[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_fdp | bash"
