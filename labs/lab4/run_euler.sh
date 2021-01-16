5807e0f7dd3302b65f8561d61b2f2b3373095166
Fabian Wüthrich <fabianwu@student.ethz.ch>
2021-01-16 17:57:00 +0100

    Markov Euler

--- labs/lab4/run_euler.sh
+++ labs/lab4/run_euler.sh
@@ -3,5 +3,6 @@
 #bsub -J "no[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_no | bash"
 #bsub -J "next[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_next | bash"
 #bsub -J "stride[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_stride | bash"
-bsub -J "ghb[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_ghb | bash"
-bsub -J "fbp[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_fdp | bash"
+#bsub -J "ghb[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_ghb | bash"
+#bsub -J "fbp[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_fdp | bash"
+bsub -J "markov[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_markov | bash"
