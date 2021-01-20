7d2cbf89c23d177baa7e7557a9d2441cc8d79897
Fabian Wüthrich <fabianwu@student.ethz.ch>
2021-01-20 13:17:40 +0100

    Add ipcp result

--- labs/lab4/run_euler.sh
+++ labs/lab4/run_euler.sh
@@ -7,6 +7,7 @@
 #bsub -J "fbp[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_fdp | bash"
 #bsub -J "markov[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_markov | bash"
 #bsub -J "pangloss[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_pangloss | bash"
+#bsub -J "ipcp[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_ipcp | bash"
 bsub -J "sangam_dpc3[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_sangam_dpc3 | bash"
 bsub -J "berti[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_berti | bash"
 bsub -J "team_12[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_team_12 | bash"
