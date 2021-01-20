fc211efef1309ff4a6733e01cf525e17ebafa93e
Fabian Wüthrich <me@fabwu.ch>
2021-01-20 13:14:37 +0100

    WIP

--- labs/lab4/euler/a.py
+++ labs/lab4/euler/a.py
@@ -1,33 +1,83 @@
 #!/usr/bin/env python3
 
 import sys
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-def printIf(line, prefix):
-    if line.startswith(prefix):
-        print(line)
+files = [
+        "euler/result_no.txt",
+        "euler/result_next.txt",
+        "euler/result_stride.txt",
+        "euler/result_ghb.txt",
+        "euler/result_fdb.txt",
+        "euler/result_markov.txt",
+        "euler/result_pangloss.txt",
+        ]
 
-def main(filename):
+def process_file(filename,data):
     runs = []
     with open(filename) as file:
         run = []
         for line in file:
             line = line.strip()
-            run.append(line)
+            if line.startswith("Heartbeat CPU"):
+                continue
 
-            if line == 'ChampSim Multicore Out-of-Order Simulator':
+            if line.startswith("trace_0"):
                 runs.append(run)
                 run = []
 
-    for run in runs:
-        for line in run:
-            printIf(line, 'trace_0')
-            printIf(line, 'Core_0_L2C_prefetch_requested')
-            printIf(line, 'Core_0_L2C_prefetch_issued')
-            printIf(line, 'Core_0_L2C_prefetch_useful')
-            printIf(line, 'Core_0_L2C_prefetch_useless')
-            printIf(line, 'Core_0_L2C_prefetch_late')
+            run.append(line)
+        runs.append(run)
 
-        print()
+    for run in runs:
+        row = []
+        row.append(filename)
+        for line in run:
+            if len(line.split()) != 2:
+                continue
+
+            key, value = line.split()
+            if key == 'trace_0':
+                #print(filename + ": "+ line)
+                trace = value.split(".")[1]
+                trace = trace.split("-")[0]
+                trace = trace.split("_")[0]
+                row.append(trace)
+
+            if key == 'Core_0_IPC':
+                row.append(float(value))
+
+            if key in ['Core_0_L2C_prefetch_requested',
+                       'Core_0_L2C_prefetch_issued',
+                       'Core_0_L2C_prefetch_useful',
+                       'Core_0_L2C_prefetch_useless',
+                       'Core_0_L2C_prefetch_late',
+                       ]:
+                row.append(int(value))
+
+        if len(row) > 1:
+            data.append(row)
+
+def main():
+    data = []
+    for filename in files:
+        process_file(filename, data);
+
+    df = pd.DataFrame(data, columns=["prefetcher",
+                                     "trace",
+                                     "ipc",
+                                     "pre_requested",
+                                     "pre_issued",
+                                     "pre_useful",
+                                     "pre_useless",
+                                     "pre_late",
+                                     ])
+    #print(df)
+    sns.catplot(x="trace", y="ipc", hue="prefetcher", data=df, kind="bar")
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main()
--- labs/lab4/run_euler.sh
+++ labs/lab4/run_euler.sh
@@ -6,4 +6,7 @@
 #bsub -J "ghb[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_ghb | bash"
 #bsub -J "fbp[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_fdp | bash"
 #bsub -J "markov[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_markov | bash"
-bsub -J "pangloss[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_pangloss | bash"
+#bsub -J "pangloss[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_pangloss | bash"
+bsub -J "sangam_dpc3[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_sangam_dpc3 | bash"
+bsub -J "berti[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_berti | bash"
+bsub -J "team_12[1-16]" -N "awk -v jindex=\$LSB_JOBINDEX 'NR==jindex' euler/commands_team_12 | bash"
