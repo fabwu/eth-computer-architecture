c46d919c1aadb982ea43aa076ba594f87c469dfd
Fabian Wüthrich <me@fabwu.ch>
2021-01-25 11:19:01 +0100

    Add lab4

--- labs/lab4/euler/a.py
+++ labs/lab4/euler/a.py
@@ -7,15 +7,16 @@ import matplotlib.pyplot as plt
 
 files = [
         "euler/result_no.txt",
-        "euler/dpc3/result_pangloss.txt",
-        "euler/dpc3/result_ipcp.txt",
-        "euler/dpc3/result_sangam.txt",
-        "euler/dpc3/result_berti.txt",
-        "euler/dpc3/result_team_12.txt",
-        "euler/result_next.txt",
-        "euler/result_stride.txt",
+#        "euler/dpc3/result_pangloss.txt",
+#        "euler/dpc3/result_ipcp.txt",
+#        "euler/dpc3/result_sangam.txt",
+#        "euler/dpc3/result_berti.txt",
+#        "euler/dpc3/result_team_12.txt",
+#        "euler/result_next.txt",
+#        "euler/result_stride.txt",
         "euler/result_ghb.txt",
         "euler/result_fdb.txt",
+        "euler/result_markov.txt",
         ]
 
 def process_file(filename,data):
@@ -36,7 +37,9 @@ def process_file(filename,data):
 
     for run in runs:
         row = []
-        row.append(filename)
+        prefetcher = filename.split(".")[0]
+        prefetcher = prefetcher.split("_")[1]
+        row.append(prefetcher)
         for line in run:
             if len(line.split()) != 2:
                 continue
@@ -69,13 +72,24 @@ def main():
         process_file(filename, data);
 
     for prefetcher in files:
+        prefetcher = prefetcher.split(".")[0]
+        prefetcher = prefetcher.split("_")[1]
+
         product = 1.0
         num_el = 0
         for line in data:
             if prefetcher == line[0]:
                 product *= line[2]
                 num_el += 1
-        print(prefetcher + ": " + str(pow(product, 1/num_el)))
+        mean_ipc = pow(product, 1/num_el)
+        data.append([prefetcher, "Avg.", mean_ipc])
+        print(prefetcher + ": " + str(mean_ipc))
+
+    no_lines = [l.copy() for l in data if l[0] == "no"]
+    for no_l in no_lines:
+        for line in data:
+            if line[1] == no_l[1]:
+                line[2] = line[2] / no_l[2]
 
     df = pd.DataFrame(data, columns=["prefetcher",
                                      "trace",
@@ -86,10 +100,18 @@ def main():
                                      "pre_useless",
                                      "pre_late",
                                      ])
-    #print(df)
-    sns.catplot(x="trace", y="ipc", hue="prefetcher", data=df, kind="bar")
-    plt.tight_layout()
-    plt.show()
+    print(df)
+    sns.set_style("whitegrid")
+    sns.set_context("paper")
+    fig, ax = plt.subplots(figsize=(16, 6))
+    g = sns.barplot(x="trace", y="ipc", hue="prefetcher", data=df, ax=ax)
+
+    ax.set_xlabel("")
+    ax.set_ylabel("IPC (normalized)")
+    plt.tight_layout(pad=0.4)
+    plt.legend(loc="upper left", title="Prefetcher")
+    plt.savefig(f"report/img/plot.pdf", format="pdf")
+    #plt.show()
 
 if __name__ == "__main__":
     main()
